52b7c3826207d99305a03ae54b1dffe42d8fe744
dada-2019 <122332627+dada-2019@users.noreply.github.com>
2025-01-09 13:34:48 +0900

    html, css, js 新規作成

---
+++ app.js
@@ -0,0 +1,12 @@
+const x = 5;
+const y = 10;
+console.log(x + y);
+
+const list = [1, 2, 3, 4, 5];
+console.log(list);
+
+const obj = {
+    name: 'John',
+    age: 30,
+    };
+console.log(obj.name, obj.age);

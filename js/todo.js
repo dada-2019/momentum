96ef434a558d09c10ef95e8b5c82058ebe9893d1
dada-2019 <122332627+dada-2019@users.noreply.github.com>
2025-01-21 11:50:58 +0900

    adding array

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,12 @@
 const toDoForm =document.querySelector("#todo-form");
 const todoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#to-list");
+const TODOS_KEY = "todos";
 const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function deleteToDo (event){
     const li = event.target.parentElement;
@@ -26,8 +31,16 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;    
     todoInput.value = "";
-    toDos.push(newTodo);
+    toDos.push(newTodo);       //local storage에 저장하기위해 배열에 일단 먼저 저장
     paintToDo (newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const getSavetoDos = localStorage.getItem(TODOS_KEY);
+if(getSavetoDos !== null){
+    const pasredToDos = JSON.parse(getSavetoDos);  //local storage에 저장된 값을 가져와서 배열로 변환
+    pasredToDos.forEach((item) => console.log("this is the turn of", item));
+
+}

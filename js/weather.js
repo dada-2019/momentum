904f5e827bae4ffddddc571cce98d28ad58ab2bb
dada-2019 <122332627+dada-2019@users.noreply.github.com>
2025-01-28 17:56:13 +0900

    done

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+
+
+function onGeoOk(Position) {
+    const lat = Position.coords.latitude;
+    const lng = Position.coords.longitude;
+    console.log("You live in", lat, lng);
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).
+        then((response) => response.json()).
+        then((data) => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.name; 
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}   
+function onGeoError() {
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+// https://openweathermap.org/  API 
+// username : dada2019, tarawameman@gmail.com, kkk1-5
